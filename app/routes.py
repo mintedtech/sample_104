0fda00869641552aa4daa4df4458b74657de18ca
sa <mintedtech@gmail.com>
2019-12-02 09:13:29 -0500

    Initial Commit

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+from flask import render_template
+
+# If a user tries to access either no page# (e.g. www.touro.edu/)
+# or the index page, then call index()
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title="Home Page")
