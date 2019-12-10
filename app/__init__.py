bda1fb75cb722bda9a4462513f1770ae08039c93
SA <mintedtech@gmail.com>
2019-12-10 15:17:32 -0500

    Added more styles from CSS Text and Box chapters

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__, template_folder='../templates')
 app.static_folder = '../static'
 
 # This import must come AFTER the statement: app = Flask (__name__)
-# The reason for this is that routes.py, above,
-# refers to the app variable above. So, the import must be after this app=
+# The reason for this is that routes.py refers to the app variable above.
+# So, the import must be after this app= variable so that routes can refer to the app variable, above.
 # That's why this import isn’t at the top, unlike PyCharm's recommendation
 # This tells Python to check the routes file in this app package
 # This file determines what function each HTML route/page should run
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 
+
 # If a user tries to access either no page# (e.g. www.touro.edu/)
 # or the index page, then call index()
 @app.route('/')
